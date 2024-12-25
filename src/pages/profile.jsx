4f05623b6e9cc898b7d428deac7b7af876946072
Sri Shasti <srishastiap@gmail.com>
2024-12-25 20:41:49 +0530

    feat(user): add profile and register page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,38 @@
+import {useEffect} from 'react'
+import {useState} from 'react'
+import axios from 'axios'
 
 const Profile = () => {
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+
+    const [user, setUser] = useState({});
+
+    useEffect (
+        () => {
+
+            axios.get(`${apiUrl}/profile`, {withCredentials : true})
+            .then ((response) => {
+                setUser(response.data);
+            })
+            .catch((error)=>{ console.log("error") })
+    
+        }
+    ,[])
 
     return (
         <div>
 
-            <h1> Profile </h1>
+            
+            <h1> {user.username} </h1>
+            <br />
+            <h3> Level : {user.level} </h3>
+            <label> XP </label> <br/>
+            <progress value = {user.xp} max = "200"> </progress> <span>{user.xp}/200</span>
+            <br/><br/>
+            <label> Health </label><br/>
+            <progress value = {user.health} max = "100"> {user.health} </progress> <span>{user.health}/100</span>
+ 
 
         </div>
     );
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,8 +1,64 @@
+import { useState } from "react"
+import axios from 'axios'
+import { NavLink } from "react-router-dom";
+import {useNavigate} from 'react-router-dom'
 
 const Register = () => {
+
+    const apiUrl = import.meta.env.VITE_API_URL;
+    const navigate = useNavigate();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e) => {
+        
+        e.preventDefault();
+
+        const credentials = {
+            "username" : username,
+            "password" : password,
+        }
+
+        try{
+            await axios.post(`${apiUrl}/register`,credentials, 
+                {
+                    headers : {'Content-Type':'application/json'},
+                    withCredentials : true,
+                })
+            console.log("user added!");
+            navigate('/tasks');
+
+        }
+        catch(error) {
+            console.log("error");
+            if(error.response && error.response.status === 409) alert("Account with the username aleady exists");
+            setUsername("");
+            setPassword("");
+        }
+        
+
+
+    }
+
     return (
         <div>
-            Register
+            <h1> Register </h1>
+
+            <form onSubmit={handleSubmit}>
+
+            <label> Username </label>
+            <input type = "text" value = {username} onChange={(e) => setUsername(e.target.value)} required></input>
+            <br/>
+            <label> Password </label>
+            <input type= "text" value = {password} onChange={(e) => setPassword(e.target.value)} required></input>
+            <br/>
+            <button type="submit"> Register </button>
+
+            </form>
+            
+            <br/><br/>
+            <label> Have an account? <NavLink to='/login'> <u> Login </u> </NavLink></label>
         </div>
     );
 };
